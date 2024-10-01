aa0454dfba8b2588643d53d42de7bd64e0d64e79
Madhini Maheshika <127973368+mdh00@users.noreply.github.com>
2024-10-01 22:27:22 +0530

    feat: home links

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero";
+import Dashboard from "./components/dashboard";
 
 function HomePage() {
   return (
     <main>
       <div>
-        <Hero/>
+        <Hero />
+      </div>
+      <div className="mt-6">
+        <Dashboard />
       </div>
     </main>
   );
